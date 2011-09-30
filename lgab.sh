79f7be4b5e6d43d2d13502bef14b08757ba605c8
Patrick Haller <patrick_haller@ofs.edu.sg>
2011-09-30 10:35:23 +0800

    names names names

--- lgab.sh
+++ lgab.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # little green advanced bash
 
-lgab_is_running_pid() { ps -p $1  2>&1 >/dev/null ; }
-lgab_is_running_pidfile() { lgab_is_running_pid $(< "$1" ) ; }
-lgab_is_running_command() {
+lgab_is_process_pid() { ps -p $1  2>&1 >/dev/null ; }
+lgab_is_process_pidfile() { lgab_is_process_pid $(< "$1" ) ; }
+lgab_is_process_command() {
 	cmd=$( basename "$1" )
 	ps -C $cmd 2>&1 >/dev/null
 }
-lgab_is_running() {
-	[[ $1 =~ '^[0-9]*$' ]] && { lgab_is_running_pid $1 ; return $?; }
-	[[ -x $1 ]] && { lgab_is_running_command $1 ; return $?; }
-	[[ -e $1 ]] && { lgab_is_running_pidfile $1 ; return $?; }
-	lgab_is_running_command $1 
+lgab_is_process() {
+	[[ $1 =~ '^[0-9]*$' ]] && { lgab_is_process_pid $1 ; return $?; }
+	[[ -x $1 ]] && { lgab_is_process_command $1 ; return $?; }
+	[[ -e $1 ]] && { lgab_is_process_pidfile $1 ; return $?; }
+	lgab_is_process_command $1 
 	return $?
 }
 
@@ -26,9 +26,9 @@ lgab_test_run() {
 lgab_test_suite() {
 	tmpf="/tmp/lgab-pid-test"
 	echo 1 > $tmpf
-	lgab_test_run 'lgab_is_running "bash"' "" 0
-	lgab_test_run 'lgab_is_running 1' "" 0
-	lgab_test_run "lgab_is_running $tmpf" "" 0
+	lgab_test_run 'lgab_is_process "bash"' "" 0
+	lgab_test_run 'lgab_is_process 1' "" 0
+	lgab_test_run "lgab_is_process $tmpf" "" 0
 	rm -f $tmpf
 }
 
